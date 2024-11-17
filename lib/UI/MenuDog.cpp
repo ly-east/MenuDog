f0749a37b462ada7e840501cd9bf17e349693772
Daru <ozziewei@gmail.com>
2024-11-17 16:13:55 +0800

    optimize layout

--- lib/UI/MenuDog.cpp
+++ lib/UI/MenuDog.cpp
@@ -1,5 +1,8 @@
 #include "UI/MenuDog.h"
 #include "ui_MenuDog.h"
+
+#include "Version.h"
+#include <QString>
 #include <QTableWidget>
 
 MenuDog::MenuDog(QWidget *parent) : QMainWindow{parent}, ui{new Ui::MenuDog} {
@@ -10,6 +13,22 @@ MenuDog::MenuDog(QWidget *parent) : QMainWindow{parent}, ui{new Ui::MenuDog} {
   setFixedSize(width(), height());
 
   ui->statusbar->setSizeGripEnabled(false);
+
+  // hide win11 context table. functions related may be implemented in the
+  // future
+  ui->menu_win11->hide();
+
+  // default tab
+  ui->tabWidget->setCurrentIndex(0);
+
+  // About tab
+
+  QString version =
+      QString("Version: V%1R%2C%3")
+          .arg(PROJECT_VERSION_MAJOR * 100, 3, 10, QLatin1Char('0'))
+          .arg(PROJECT_VERSION_MINOR, 3, 10, QLatin1Char('0'))
+          .arg(PROJECT_VERSION_PATCH, 2, 10, QLatin1Char('0'));
+  ui->version->setText(version);
 }
 
 MenuDog::~MenuDog() { delete ui; }
