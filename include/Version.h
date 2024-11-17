7a58c95ee07e3deeabf2c4779006754c105aefd5
Daru <ozziewei@gmail.com>
2024-11-17 12:36:31 +0800

    display version

---
+++ include/Version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// clang-format off
+#define PROJECT_VERSION_MAJOR 0
+#define PROJECT_VERSION_MINOR 1
+#define PROJECT_VERSION_PATCH 0
+#define PROJECT_VERSION "0.1.0"
+// clang-format on
+
+#endif // VERSION_H
--- lib/UI/Loading.cpp
+++ lib/UI/Loading.cpp
@@ -1,4 +1,5 @@
 #include "UI/Loading.h"
+#include "Version.h"
 #include "ui_Loading.h"
 
 Loading::Loading(QWidget *parent) : QMainWindow{parent}, ui{new Ui::Loading} {
@@ -9,7 +10,13 @@ Loading::Loading(QWidget *parent) : QMainWindow{parent}, ui{new Ui::Loading} {
   ui->label->setPixmap(QPixmap(":/img/image.jpg").scaled(400, 280));
 
   ui->statusbar->setSizeGripEnabled(false);
-  ui->statusbar->addWidget(new QLabel("Initializing...Please wait", this));
+  ui->statusbar->addWidget(new QLabel("Initializing...Please Wait\t", this));
+  ui->statusbar->addWidget(
+      new QLabel(QString("Version: V%1R%2C%3")
+                     .arg(PROJECT_VERSION_MAJOR * 100, 3, 10, QLatin1Char('0'))
+                     .arg(PROJECT_VERSION_MINOR, 3, 10, QLatin1Char('0'))
+                     .arg(PROJECT_VERSION_PATCH, 2, 10, QLatin1Char('0')),
+                 this));
 }
 
 Loading::~Loading() { delete ui; }
