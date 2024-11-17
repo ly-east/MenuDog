c37a330eb8ba77af36fc3228848dfdd64c6b04da
Daru <ozziewei@gmail.com>
2024-11-17 15:01:56 +0800

    display splashscreen when loading

--- lib/UI/main.cpp
+++ lib/UI/main.cpp
@@ -1,7 +1,9 @@
 #include "UI/Loading.h"
+#include "UI/MenuDog.h"
 #include "ucrash/ucrash.h"
 #include "ulog/ulog.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[]) {
   // set file logger
@@ -13,8 +15,25 @@ int main(int argc, char *argv[]) {
 
   QApplication a{argc, argv};
 
-  Loading l;
-  l.show();
+  QPixmap pixmap{":/img/image.jpg"};
+  QSplashScreen splash{pixmap.scaled(400, 280)};
+  splash.show();
+
+  a.processEvents();
+
+  // TODO: load CLSID
+  splash.showMessage(QStringLiteral("Loading CLSID..."),
+                     Qt::AlignHCenter | Qt::AlignBottom, Qt::white);
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  // TODO: load rules
+  splash.showMessage(QStringLiteral("Loading Rules..."),
+                     Qt::AlignHCenter | Qt::AlignBottom, Qt::white);
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  MenuDog md;
+  md.show();
+  splash.finish(&md);
 
   return a.exec();
 }
